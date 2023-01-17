3147c14ad89f9ce1facaa4e856d2c0ad2b3fa255
LIUQI <1139642444@qq.com>
2023-01-17 15:53:08 +0800

    新增 router

---
+++ src/router/index.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+// 创建并挂载根实例
+const app = Vue.createApp({})
+app.use(Router)
---
+++ src/views/login.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="login">
+            TGGGD
+    </div>
+</template>
