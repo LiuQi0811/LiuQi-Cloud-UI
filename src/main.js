703617b2c099723f113ba3a1f1a5aad125adc861
LIUQI <1139642444@qq.com>
2023-01-17 16:44:52 +0800

    route 优化 vue router 4

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App,router).mount('#app')
+
+createApp(App)
+//确保 _use_ 路由实例使整个应用支持路由
+.use(router)
+.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,23 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import {createRouter,createWebHistory} from 'vue-router'
 
-// 创建并挂载根实例
-const app = Vue.createApp({})
-app.use(Router)
+
+// 路由信息
+const constantRoutes = [
+    {
+        path: '/',
+        component: () => import('@/components/HelloWorld.vue')
+    },
+    {
+        path: '/login',
+        component: () => import('@/views/login.vue'),
+        hidden: true
+      }
+]
+
+// 路由器
+const router = createRouter({
+    history: createWebHistory(), // HTML5模式
+    routes: constantRoutes
+  });
+
+export default router;
