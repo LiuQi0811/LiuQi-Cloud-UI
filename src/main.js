a4dfb1b97d9d60696f1ae61cae41a14baec75e35
LIUQI <1139642444@qq.com>
2023-01-18 09:16:47 +0800

    vue3 使用 图标

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIcons from '@element-plus/icons-vue'
 import router from './router'
 
 // 创建并挂载根实例
@@ -9,4 +10,8 @@ const app =  createApp(App)
 //确保 _use_ 路由实例使整个应用支持路由
 app.use(router)
 app.use(ElementPlus)
+// 图标注册
+for (const name in ElementPlusIcons){
+	app.component(name,ElementPlusIcons[name])
+}
 app.mount('#app')
--- src/views/login.vue
+++ src/views/login.vue
@@ -4,18 +4,48 @@
         <h3>LQ后台管理系统</h3>
         <!-- 用户名 -->
        <el-form-item prop="username">
-        <el-input v-model="loginForm.username" placeholder="账号"></el-input>
+        <el-input v-model="loginForm.username" placeholder="账号">
+            <template #prefix>
+                <!--   vue3图标使用方式  -->
+                <el-icon>
+                    <User />
+                </el-icon>
+            </template>
+        </el-input>
        </el-form-item>
        <!-- 密码 -->
        <el-form-item prop="password">
-        <el-input type="password" v-model="loginForm.password" placeholder="密码"></el-input>
+        <el-input type="password" v-model="loginForm.password" placeholder="密码">
+            <template #prefix>
+                <!--   vue3图标使用方式  -->
+                <el-icon>
+                    <Lock />
+                </el-icon>
+            </template>
+        </el-input>
        </el-form-item>
        <!-- 验证码 -->
        <el-form-item prop="verificationCode">
-        <el-input v-model="loginForm.verificationCode" placeholder="验证码"></el-input>
-        <div class="login-code">
+        <el-input v-model="loginForm.verificationCode" placeholder="验证码"   style="width: 63%">
+            <template #prefix>
+                <!--   vue3图标使用方式  -->
+                <el-icon>
+                    <CircleCheck />
+                </el-icon>
+            </template>
+        </el-input>
+        <div class="login-verification-code">
           <img :src="verificationCodeUrl"  class="login-verification-code-img"/>
         </div>
+       </el-form-item>
+       <!-- 记住密码 -->
+       <el-checkbox style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+       <!-- 登录 -->
+       <el-form-item>
+        <el-button type="primary">
+            <span>登 录</span>
+            <span>登 录 中...</span>
+        </el-button>
        </el-form-item>
      </el-form>
     </div>
